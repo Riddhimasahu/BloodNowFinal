4aa7c6f67d9112dea3ae1ff0eb2e10640f434dfb
Riddhima Sahu <riddhimasahu172005@gmail.com>
2026-04-04 01:54:10 +0530

    commit

---
+++ lib/models/onboarding_feature.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class OnboardingFeature {
+  const OnboardingFeature({
+    required this.title,
+    required this.body,
+    required this.icon,
+  });
+
+  final String title;
+  final String body;
+  final IconData icon;
+}
